84639212020c6a7b14d6308e8cd104bed1d9b025
Mateus Sarmento <omateusmento@gmail.com>
2021-07-23 11:49:38 -0300

    Use name prop to filter named fillings

--- src/Slot.js
+++ src/Slot.js
@@ -1,10 +1,22 @@
 function Slot(props) {
-	let { 
+	let {
+		$name: name,
 		$source: source = null,
 		...defaultProps
 	} = props;
 
-	return source.filter(s => !Object.keys(s.props).some(k => k.startsWith("$")));
+	if (name)
+		return renderAsNamedSlot(name, source);
+
+	return renderAsDefaultSlot(source);
+}
+
+function renderAsDefaultSlot(fillings) {
+	return fillings.filter(f => !Object.keys(f.props).some(k => k.startsWith("$")));
+}
+
+function renderAsNamedSlot(name, fillings) {
+	return fillings.filter(f => Object.keys(f.props).some(k => k === "$" + name));
 }
 
 export default Slot;
