b27ac9d39ab4de6025ad134e6f8306defe30f3f5
Mateus Sarmento <omateusmento@gmail.com>
2021-07-26 09:46:52 -0300

    Import polyfill in setupTests

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,6 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
 import '@testing-library/jest-dom';
